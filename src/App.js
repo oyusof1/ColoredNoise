4f8e3e25cdc2afc3c9adb25a392adb51f7e551b8
osman <oyusof73@gmail.com>
2022-11-25 17:19:33 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import cube from './assets/cube.gif';
+import spin from './assets/spin.gif';
+import glitch from './assets/glitch.gif';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [logo, setLogo] = useState(cube);
+
+  const showLogo = (logo) => {
+    setLogo(logo);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          C0M1NG S00N!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='buttons'>
+          <button onClick={() => showLogo(spin)}>SP1N</button>
+          <button onClick={() => showLogo(cube)}>CUB3</button>
+          <button onClick={() => showLogo(glitch)}>GL1TCH</button>
+        </div>
     </div>
   );
 }
